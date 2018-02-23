8ee821927bd1b8a6ab5264cede7242bb45c58109
Tech-A66 <tech-a66@MacBook-Air-10.local>
2018-02-23 10:40:18 +0000

    square clicked

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
-$function(){
+$ (function(){
+
 	console.log("all set");
-}
+	var squares = $('td');
+	var resetButton = $('#reset');
+	var stepCounter = 0;
+	squares.on("click" , function(event){
+
+		if(stepCounter %2 == 0){
+			$(this).html("X");
+		}else{
+			$(this).html("0");
+		}
+		//$(this).html("X");
+		stepCounter ++ //could also be ++stepCounter (means the same)
+	});
+
+	resetButton.on("click", function(event){
+		squares.html("");
+	});
+
+
+
+});
